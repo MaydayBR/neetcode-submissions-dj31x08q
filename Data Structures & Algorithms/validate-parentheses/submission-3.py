eaa2e88c15df025d3e5a88107914513a11d5c554
MaydayBR <bmayday20@gmail.com>
2026-04-02 12:58:53 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        opening = {"(" , "[" , "{"}
+        closing = {")":"(" , "]":"[" , "}":"{"}
+        stack = []
+
+        for char in s:
+            if char in opening:
+                stack.append(char)
+            elif char in closing:
+                if len(stack) < 1:
+                    return False
+                character_removed = stack.pop()
+                character_needed = closing[char]
+                if character_removed != character_needed:
+                    return False
+
+        return len(stack) == 0
